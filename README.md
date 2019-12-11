919c4eea773c409b40fa62a8090f26cfaf6d3883
Wes Bos <wesbos@gmail.com>
2019-12-11 10:35:40 -0500

    thats annoying

---
+++ README.md
@@ -0,0 +1,13 @@
+# iTunes Sniper
+
+![Neat](./neat.gif)
+
+Any time iTunes tries to open, kill it and open Spotify.
+
+It's a node script. Run it with `node overkill.js`.
+
+Someone make this into a startup script? Or nice little electron app?
+
+Prior Art: [Overkill.rb](https://github.com/KrauseFx/overkill/blob/master/overkill.rb)
+
+License: WTFPL
---
+++ overkill.js
@@ -0,0 +1,15 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function monitor() {
+  const { stdout } = await exec(`ps aux | grep -E "Music"`);
+  const musicApp = stdout.split('\n').find(app => app.includes('Music.app'));
+  if (musicApp) {
+    console.log(`Music launched itself, killing the process now... 💥`);
+    await exec(`killall Music`);
+    console.log(`Opening Spotify instead. Enjoy!`);
+    await exec(`open /Applications/Spotify.app `);
+  }
+}
+
+setInterval(monitor, 1000);
